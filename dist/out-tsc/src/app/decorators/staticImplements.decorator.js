3b1b90cb3dd0f7a5dcd32590935418f015c71742
Craig Burton <the.number.zero@gmail.com>
2018-12-11 04:51:24 -0500

    More work on deployment

--- dist/out-tsc/src/app/decorators/staticImplements.decorator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/* https://stackoverflow.com/a/43674389 */
-function staticImplements() {
-    return function (constructor) { };
-}
-exports.staticImplements = staticImplements;
-//# sourceMappingURL=staticImplements.decorator.js.map
